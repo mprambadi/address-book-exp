583502f8f8344494e960c80f39ce0ba67fd1321f
Maulana <mprambadi@gmail.com>
2021-06-17 13:05:09 +0700

    Updates

---
+++ precache-manifest.995c976559a7d8ab9cf6086e1937a2f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba4901dd67576b10327",
+    "url": "/address-book-exp/static/js/4.eba4901d.chunk.js"
+  },
+  {
+    "revision": "18e268e5debc036b2dd7",
+    "url": "/address-book-exp/static/js/0.18e268e5.chunk.js"
+  },
+  {
+    "revision": "4c16818b0f6dac3e7a1b",
+    "url": "/address-book-exp/static/js/runtime~main.4c16818b.js"
+  },
+  {
+    "revision": "43ae471b775dc7de2b2d",
+    "url": "/address-book-exp/static/js/2.43ae471b.chunk.js"
+  },
+  {
+    "revision": "21a2ca40e199abe671b7",
+    "url": "/address-book-exp/static/js/3.21a2ca40.chunk.js"
+  },
+  {
+    "revision": "7183270bfc87ba69b42c",
+    "url": "/address-book-exp/static/js/main.7183270b.chunk.js"
+  },
+  {
+    "revision": "128bf5e50c6224606d37",
+    "url": "/address-book-exp/static/js/5.128bf5e5.chunk.js"
+  },
+  {
+    "revision": "8ee3eb7c62e9c0ba593f",
+    "url": "/address-book-exp/static/js/6.8ee3eb7c.chunk.js"
+  },
+  {
+    "revision": "ca04eeaac91e30cf1124",
+    "url": "/address-book-exp/static/js/7.ca04eeaa.chunk.js"
+  },
+  {
+    "revision": "43ae471b775dc7de2b2d",
+    "url": "/address-book-exp/static/css/2.0be959ec.chunk.css"
+  },
+  {
+    "revision": "9f118baf9272de7196b4bc06e14e3c97",
+    "url": "/address-book-exp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.373e97d57d2e076095fd944ec8a44cd3.js"
+  "/address-book-exp/precache-manifest.995c976559a7d8ab9cf6086e1937a2f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/address-book-exp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
